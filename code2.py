5a2e343a089e7b8a7929cf5b7bb1f8c0645b8ad1
Dima-Hamratsey <151673637+Dima-Hamratsey@users.noreply.github.com>
2024-03-14 10:31:29 +0200

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,21 +1,19 @@
 def find_three_numbers(arr, P):
-    arr.sort()  # Сортуємо масив
-
+    arr.sort()
     n = len(arr)
     for i in range(n - 2):
-        left = i + 1
-        right = n - 1
+        remaining_sum = P - arr[i]
+        left, right = i + 1, n - 1
         while left < right:
-            current_sum = arr[i] + arr[left] + arr[right]
-            if current_sum == P:
+            two_sum = arr[left] + arr[right]
+            if two_sum == remaining_sum:
                 return True
-            elif current_sum < P:
+            elif two_sum < remaining_sum:
                 left += 1
-            else:
+            elif two_sum  > remaining_sum:
                 right -= 1
     return False
 
-# Приклад використання функції
 arr = [1, 2, 3]
 P = 6
-print(find_three_numbers(arr, P))  # True йопта
+print(find_three_numbers(arr, P))  # True
