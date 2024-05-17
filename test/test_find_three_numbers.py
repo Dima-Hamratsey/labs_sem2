31f8a06ddfa693eae2e095a65c3607c7715383a3
Dima-Hamratsey <151673637+Dima-Hamratsey@users.noreply.github.com>
2024-05-17 10:53:49 +0300

    Update and rename test_for_binary_tree.py to test_find_three_numbers.py

--- test/test_find_three_numbers.py
+++ test/test_find_three_numbers.py
@@ -1,5 +1,5 @@
 import unittest
-from test_binary_tree import find_three_numbers
+from src.find_three_numbers import find_three_numbers
 
 class TestFindThreeNumbers(unittest.TestCase):
     def test_case_1(self):
